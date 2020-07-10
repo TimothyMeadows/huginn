203d1bef9a7ad8c530abb5865abc86a5b2e9fa1c
Timothy D Meadows II <timothy@timothymeadows.com>
2020-07-10 16:40:54 -0700

    add huginn-vid-alpr

--- huginn-vid-alpr.py
+++ huginn-vid-alpr.py
@@ -2,3 +2,19 @@ import cv2
 import numpy
 from openalpr import Alpr
 
+alpr = Alpr("us", "/etc/openalpr/openalpr.conf",
+            "/usr/share/openalpr/runtime_data")
+
+if not alpr.is_loaded():
+    print("Error loading OpenALPR")
+else:
+    print("Using OpenALPR " + alpr.get_version())
+
+alpr.set_top_n(7)
+alpr.set_default_region("wa")
+alpr.set_detect_region(False)
+jpeg_bytes = open("images/ca-plate.png", "rb").read()
+results = alpr.recognize_array(jpeg_bytes)
+
+print("Image size: %dx%d" %(results['img_width'], results['img_height']))
+print("Processing Time: %f" % results['processing_time_ms'])
