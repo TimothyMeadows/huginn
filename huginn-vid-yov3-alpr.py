86d1a160bc4e6af7e7e4743c2cf2217441da2c66
Timothy D Meadows II <timothy@timothymeadows.com>
2020-07-10 19:56:02 -0700

    add yov3-alpr combo

--- huginn-vid-yov3-alpr.py
+++ huginn-vid-yov3-alpr.py
@@ -3,6 +3,7 @@ import argparse
 import imutils
 import time
 from pydarknet import Detector, Image
+from openalpr import Alpr
 import cv2
 import os
 
@@ -30,21 +31,54 @@ dataPath = os.path.sep.join(["yolov3", "coco.data"])
 
 net = Detector(bytes(configPath, encoding="utf-8"), bytes(weightsPath,
                                                           encoding="utf-8"), 0, bytes(dataPath, encoding="utf-8"))
+
+alpr = Alpr("gb", "/etc/openalpr/openalpr.conf",
+            "/usr/share/openalpr/runtime_data")
+
+if not alpr.is_loaded():
+    print("Error loading OpenALPR")
+    exit()
+
+alpr.set_top_n(1)
+alpr.set_default_region('gb')
+alpr.set_detect_region(False)
+
 cap = cv2.VideoCapture("videos/traffic-uk.mp4")
-#fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-#writer = cv2.VideoWriter("output.mp4", fourcc, 24, (800, 600), True)
-		
+fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+writer = cv2.VideoWriter("output.mp4", fourcc, 24, (1980, 1024), True)
+
 (W, H) = (None, None)
 if cap.isOpened():
-    cv2.namedWindow("video", cv2.WINDOW_AUTOSIZE)
+    #cv2.namedWindow("video", cv2.WINDOW_AUTOSIZE)
     while True:
         (grabbed, frame) = cap.read()
         if not grabbed:
             break
 
-        frame = resize(frame, (800, 600))
+        frame = resize(frame, (1980, 1024))
         if W is None or H is None:
             (H, W) = frame.shape[:2]
+
+        _, img_alpr = cv2.imencode('.jpg', frame)
+        results = alpr.recognize_array(img_alpr.tostring())
+        for plate in results['results']:
+            for candidate in plate['candidates']:
+                if candidate['matches_template']:
+                    x = int(min(plate['coordinates'],
+                                key=lambda ev: ev['x'])['x'])
+                    y = int(min(plate['coordinates'],
+                                key=lambda ev: ev['y'])['y'])
+                    w = int(max(plate['coordinates'],
+                                key=lambda ev: ev['x'])['x'])
+                    h = int(max(plate['coordinates'],
+                                key=lambda ev: ev['y'])['y'])
+
+                    print("Cords: " + str((x, y, w, h)) + " Plate: " + str(candidate['plate']) + " Confidence: " + str(
+                        candidate['confidence']) + " Region: " + str(plate['region']))
+
+                    cv2.rectangle(frame, (x, y), (w, h), (0, 255, 255), 2)
+                    cv2.putText(frame, str(
+                        candidate['plate']), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
 
         img_darknet = Image(frame)
         results = net.detect(img_darknet)
@@ -61,11 +95,11 @@ if cap.isOpened():
 
             overlay(frame, x, y, w, h, color, 1, name, str(round(score, 2)))
 
-        cv2.imshow("video", frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
+        #cv2.imshow("video", frame)
+        # if cv2.waitKey(1) == ord('q'):
+            # break
 
-        #writer.write(frame)
+        writer.write(frame)
 else:
     print("unable to open video")
 cap.release()
